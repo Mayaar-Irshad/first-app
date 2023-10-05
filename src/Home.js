823397422edfb3827fb643363c3c399aaf402efb
Irshad <may.irshad@gmail.com>
2023-10-05 00:03:42 -0700

    2nd commit

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("hello, people");
+    };
+
+    const handleClickAgain = (name) => {
+        console.log("hello" + name);
+    };
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button
+                onClick={() => {
+                    handleClickAgain("yaro");
+                }}
+            >
+                click me again
+            </button>
         </div>
     );
 };
